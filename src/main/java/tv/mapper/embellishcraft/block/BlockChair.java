1890b1f71134aa439b3227f36370e6200bda586e
Mapper <killermapper@outlook.com>
2019-03-24 18:29:46 +0100

    Waterlogging chairs + small wallpaper stair tweak

--- src/main/java/tv/mapper/embellishcraft/block/BlockChair.java
+++ src/main/java/tv/mapper/embellishcraft/block/BlockChair.java
@@ -1,20 +1,28 @@
 package tv.mapper.embellishcraft.block;
 
 import net.minecraft.block.Block;
+import net.minecraft.block.IBucketPickupHandler;
+import net.minecraft.block.ILiquidContainer;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.fluid.Fluid;
+import net.minecraft.fluid.IFluidState;
+import net.minecraft.init.Fluids;
 import net.minecraft.item.BlockItemUseContext;
+import net.minecraft.state.BooleanProperty;
 import net.minecraft.state.DirectionProperty;
 import net.minecraft.state.StateContainer;
+import net.minecraft.state.properties.BlockStateProperties;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.EnumHand;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.shapes.VoxelShape;
 import net.minecraft.util.math.shapes.VoxelShapes;
 import net.minecraft.world.IBlockReader;
+import net.minecraft.world.IWorld;
 import net.minecraft.world.World;
 
-public class BlockChair extends Block
+public class BlockChair extends Block implements IBucketPickupHandler, ILiquidContainer
 {
     // Need to figure a way to rotate VoxelShapes because this is just stupid
     private static final VoxelShape SOUTH_LEFT_FRONT_LEG = Block.makeCuboidShape(11.0D, 0.0D, 11.0D, 13.0D, 7.0D, 13.0D);
@@ -50,11 +58,12 @@ public class BlockChair extends Block
     private static final VoxelShape EAST_CHAIR_AABB = VoxelShapes.or(EAST_LEFT_FRONT_LEG, VoxelShapes.or(EAST_RIGHT_FRONT_LEG, VoxelShapes.or(EAST_LEFT_BACK_LEG, VoxelShapes.or(EAST_SIT, VoxelShapes.or(EAST_BACK, EAST_RIGHT_BACK_LEG)))));
 
     public static final DirectionProperty FACING = DirectionProperty.create("facing", EnumFacing.Plane.HORIZONTAL);
+    public static final BooleanProperty WATERLOGGED = BlockStateProperties.WATERLOGGED;
 
     public BlockChair(Properties properties)
     {
         super(properties);
-        this.setDefaultState(this.stateContainer.getBaseState().with(FACING, EnumFacing.NORTH));
+        this.setDefaultState(this.stateContainer.getBaseState().with(FACING, EnumFacing.NORTH).with(WATERLOGGED, Boolean.valueOf(false)));
     }
 
     public VoxelShape getShape(IBlockState state, IBlockReader worldIn, BlockPos pos)
@@ -103,6 +112,17 @@ public class BlockChair extends Block
         return false;
     }
 
+    @SuppressWarnings("deprecation")
+    public IBlockState updatePostPlacement(IBlockState stateIn, EnumFacing facing, IBlockState facingState, IWorld worldIn, BlockPos currentPos, BlockPos facingPos)
+    {
+        if(stateIn.get(WATERLOGGED))
+        {
+            worldIn.getPendingFluidTicks().scheduleTick(currentPos, Fluids.WATER, Fluids.WATER.getTickRate(worldIn));
+        }
+
+        return super.updatePostPlacement(stateIn, facing, facingState, worldIn, currentPos, facingPos);
+    }
+
     @Override
     public boolean onBlockActivated(IBlockState state, World world, BlockPos pos, EntityPlayer player, EnumHand hand, EnumFacing side, float hitX, float hitY, float hitZ)
     {
@@ -111,7 +131,7 @@ public class BlockChair extends Block
 
     public IBlockState getStateForPlacement(BlockItemUseContext context)
     {
-        return this.getDefaultState().with(FACING, context.getPlacementHorizontalFacing().getOpposite());
+        return this.getDefaultState().with(FACING, context.getPlacementHorizontalFacing().getOpposite()).with(WATERLOGGED, Boolean.valueOf(false));
     }
 
     public IBlockState getStateFromMeta(int meta)
@@ -126,6 +146,48 @@ public class BlockChair extends Block
 
     protected void fillStateContainer(StateContainer.Builder<Block, IBlockState> builder)
     {
-        builder.add(FACING);
+        builder.add(FACING, WATERLOGGED);
+    }
+
+    public Fluid pickupFluid(IWorld worldIn, BlockPos pos, IBlockState state)
+    {
+        if(state.get(WATERLOGGED))
+        {
+            worldIn.setBlockState(pos, state.with(WATERLOGGED, Boolean.valueOf(false)), 3);
+            return Fluids.WATER;
+        }
+        else
+        {
+            return Fluids.EMPTY;
+        }
+    }
+
+    @SuppressWarnings("deprecation")
+    public IFluidState getFluidState(IBlockState state)
+    {
+        return state.get(WATERLOGGED) ? Fluids.WATER.getStillFluidState(false) : super.getFluidState(state);
+    }
+
+    public boolean canContainFluid(IBlockReader worldIn, BlockPos pos, IBlockState state, Fluid fluidIn)
+    {
+        return !state.get(WATERLOGGED) && fluidIn == Fluids.WATER;
+    }
+
+    public boolean receiveFluid(IWorld worldIn, BlockPos pos, IBlockState state, IFluidState fluidStateIn)
+    {
+        if(!state.get(WATERLOGGED) && fluidStateIn.getFluid() == Fluids.WATER)
+        {
+            if(!worldIn.isRemote())
+            {
+                worldIn.setBlockState(pos, state.with(WATERLOGGED, Boolean.valueOf(true)), 3);
+                worldIn.getPendingFluidTicks().scheduleTick(pos, fluidStateIn.getFluid(), fluidStateIn.getFluid().getTickRate(worldIn));
+            }
+
+            return true;
+        }
+        else
+        {
+            return false;
+        }
     }
 }
