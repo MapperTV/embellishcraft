2d2309c2cc7f581e46570ee41d197dcf164c17b5
Mapper <killermapper@outlook.com>
2019-10-15 12:54:18 +0200

    Minor fixes on chair block.

--- src/main/java/tv/mapper/embellishcraft/block/ChairBlock.java
+++ src/main/java/tv/mapper/embellishcraft/block/ChairBlock.java
@@ -34,6 +34,10 @@ public class ChairBlock extends Block implements IWaterLoggable
     private static final VoxelShape SOUTH_BACK = Block.makeCuboidShape(5.0D, 12.0D, 3.0D, 11.0D, 14.0D, 4.0D);
     private static final VoxelShape SOUTH_CHAIR_AABB = VoxelShapes.or(SOUTH_LEFT_FRONT_LEG, VoxelShapes.or(SOUTH_RIGHT_FRONT_LEG, VoxelShapes.or(SOUTH_LEFT_BACK_LEG, VoxelShapes.or(SOUTH_SIT, VoxelShapes.or(SOUTH_BACK, SOUTH_RIGHT_BACK_LEG)))));
 
+    private static final VoxelShape SOUTH_COL_BACK = Block.makeCuboidShape(3.0D, 0.0D, 2.0D, 13.0D, 16.0D, 4.0D);
+    private static final VoxelShape SOUTH_COL_SIT = Block.makeCuboidShape(3.0D, 0.0D, 4.0D, 13.0D, 8.0D, 13.0D);
+    private static final VoxelShape SOUTH_COL = VoxelShapes.or(SOUTH_COL_BACK, SOUTH_COL_SIT);
+
     private static final VoxelShape WEST_LEFT_FRONT_LEG = Block.makeCuboidShape(3.0D, 0.0D, 11.0D, 5.0D, 7.0D, 13.0D);
     private static final VoxelShape WEST_RIGHT_FRONT_LEG = Block.makeCuboidShape(3.0D, 0.0D, 3.0D, 5.0D, 7.0D, 5.0D);
     private static final VoxelShape WEST_LEFT_BACK_LEG = Block.makeCuboidShape(12.0D, 0.0D, 11.0D, 14.0D, 16.0D, 13.0D);
@@ -41,6 +45,10 @@ public class ChairBlock extends Block implements IWaterLoggable
     private static final VoxelShape WEST_SIT = Block.makeCuboidShape(3.0D, 7.0D, 3.0D, 12.0D, 8.0D, 13.0D);
     private static final VoxelShape WEST_BACK = Block.makeCuboidShape(12.0D, 12.0D, 5.0D, 13.0D, 14.0D, 11.0D);
     private static final VoxelShape WEST_CHAIR_AABB = VoxelShapes.or(WEST_LEFT_FRONT_LEG, VoxelShapes.or(WEST_RIGHT_FRONT_LEG, VoxelShapes.or(WEST_LEFT_BACK_LEG, VoxelShapes.or(WEST_SIT, VoxelShapes.or(WEST_BACK, WEST_RIGHT_BACK_LEG)))));
+
+    private static final VoxelShape WEST_COL_BACK = Block.makeCuboidShape(12.0D, 0.0D, 3.0D, 14.0D, 16.0D, 13.0D);
+    private static final VoxelShape WEST_COL_SIT = Block.makeCuboidShape(3.0D, 0.0D, 3.0D, 12.0D, 8.0D, 13.0D);
+    private static final VoxelShape WEST_COL = VoxelShapes.or(WEST_COL_BACK, WEST_COL_SIT);
 
     private static final VoxelShape NORTH_LEFT_FRONT_LEG = Block.makeCuboidShape(3.0D, 0.0D, 3.0D, 5.0D, 7.0D, 5.0D);
     private static final VoxelShape NORTH_RIGHT_FRONT_LEG = Block.makeCuboidShape(11.0D, 0.0D, 3.0D, 13.0D, 7.0D, 5.0D);
@@ -50,6 +58,10 @@ public class ChairBlock extends Block implements IWaterLoggable
     private static final VoxelShape NORTH_BACK = Block.makeCuboidShape(5.0D, 12.0D, 12.0D, 11.0D, 14.0D, 13.0D);
     private static final VoxelShape NORTH_CHAIR_AABB = VoxelShapes.or(NORTH_LEFT_FRONT_LEG, VoxelShapes.or(NORTH_RIGHT_FRONT_LEG, VoxelShapes.or(NORTH_LEFT_BACK_LEG, VoxelShapes.or(NORTH_SIT, VoxelShapes.or(NORTH_BACK, NORTH_RIGHT_BACK_LEG)))));
 
+    private static final VoxelShape NORTH_COL_BACK = Block.makeCuboidShape(3.0D, 0.0D, 12.0D, 13.0D, 16.0D, 14.0D);
+    private static final VoxelShape NORTH_COL_SIT = Block.makeCuboidShape(3.0D, 0.0D, 3.0D, 13.0D, 8.0D, 12.0D);
+    private static final VoxelShape NORTH_COL = VoxelShapes.or(NORTH_COL_BACK, NORTH_COL_SIT);
+
     private static final VoxelShape EAST_LEFT_FRONT_LEG = Block.makeCuboidShape(11.0D, 0.0D, 3.0D, 13.0D, 7.0D, 5.0D);
     private static final VoxelShape EAST_RIGHT_FRONT_LEG = Block.makeCuboidShape(11.0D, 0.0D, 11.0D, 13.0D, 7.0D, 13.0D);
     private static final VoxelShape EAST_LEFT_BACK_LEG = Block.makeCuboidShape(2.0D, 0.0D, 3.0D, 4.0D, 16.0D, 5.0D);
@@ -57,6 +69,10 @@ public class ChairBlock extends Block implements IWaterLoggable
     private static final VoxelShape EAST_SIT = Block.makeCuboidShape(4.0D, 7.0D, 3.0D, 13.0D, 8.0D, 13.0D);
     private static final VoxelShape EAST_BACK = Block.makeCuboidShape(3.0D, 12.0D, 5.0D, 4.0D, 14.0D, 11.0D);
     private static final VoxelShape EAST_CHAIR_AABB = VoxelShapes.or(EAST_LEFT_FRONT_LEG, VoxelShapes.or(EAST_RIGHT_FRONT_LEG, VoxelShapes.or(EAST_LEFT_BACK_LEG, VoxelShapes.or(EAST_SIT, VoxelShapes.or(EAST_BACK, EAST_RIGHT_BACK_LEG)))));
+
+    private static final VoxelShape EAST_COL_BACK = Block.makeCuboidShape(2.0D, 0.0D, 3.0D, 4.0D, 16.0D, 13.0D);
+    private static final VoxelShape EAST_COL_SIT = Block.makeCuboidShape(4.0D, 0.0D, 3.0D, 13.0D, 8.0D, 13.0D);
+    private static final VoxelShape EAST_COL = VoxelShapes.or(EAST_COL_BACK, EAST_COL_SIT);
 
     public static final DirectionProperty FACING = DirectionProperty.create("facing", Direction.Plane.HORIZONTAL);
     public static final BooleanProperty WATERLOGGED = BlockStateProperties.WATERLOGGED;
@@ -100,15 +116,15 @@ public class ChairBlock extends Block implements IWaterLoggable
         switch((Direction)state.get(FACING))
         {
             case NORTH:
-                return NORTH_CHAIR_AABB;
+                return NORTH_COL;
             case SOUTH:
-                return SOUTH_CHAIR_AABB;
+                return SOUTH_COL;
             case WEST:
-                return WEST_CHAIR_AABB;
+                return WEST_COL;
             case EAST:
-                return EAST_CHAIR_AABB;
+                return EAST_COL;
             default:
-                return SOUTH_CHAIR_AABB;
+                return SOUTH_COL;
         }
     }
 
@@ -142,7 +158,7 @@ public class ChairBlock extends Block implements IWaterLoggable
         {
             worldIn.getPendingFluidTicks().scheduleTick(currentPos, Fluids.WATER, Fluids.WATER.getTickRate(worldIn));
         }
-        
+
         if(facing == Direction.DOWN && !this.isValidPosition(stateIn, worldIn, currentPos))
             return Blocks.AIR.getDefaultState();
 
@@ -152,7 +168,10 @@ public class ChairBlock extends Block implements IWaterLoggable
     @Override
     public BlockState getStateForPlacement(BlockItemUseContext context)
     {
-        return this.getDefaultState().with(FACING, context.getPlacementHorizontalFacing().getOpposite()).with(WATERLOGGED, Boolean.valueOf(false));
+        BlockPos blockpos = context.getPos();
+        IFluidState ifluidstate = context.getWorld().getFluidState(blockpos);
+
+        return this.getDefaultState().with(FACING, context.getPlacementHorizontalFacing().getOpposite()).with(WATERLOGGED, Boolean.valueOf(Boolean.valueOf(ifluidstate.getFluid() == Fluids.WATER)));
     }
 
     protected void fillStateContainer(StateContainer.Builder<Block, BlockState> builder)
