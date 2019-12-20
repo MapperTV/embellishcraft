1f73ba6dd96a24d30beb91f8f5ddb73d1fdabb12
Mapper <killermapper@outlook.com>
2019-12-20 17:46:53 +0100

    Fancy bed model and collisions done. Now to make all textures...

--- src/main/java/tv/mapper/embellishcraft/block/CustomBedBlock.java
+++ src/main/java/tv/mapper/embellishcraft/block/CustomBedBlock.java
@@ -1,8 +1,16 @@
 package tv.mapper.embellishcraft.block;
 
 import net.minecraft.block.BedBlock;
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
 import net.minecraft.item.DyeColor;
+import net.minecraft.state.properties.BedPart;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.Direction;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.shapes.ISelectionContext;
+import net.minecraft.util.math.shapes.VoxelShape;
+import net.minecraft.util.math.shapes.VoxelShapes;
 import net.minecraft.world.IBlockReader;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
@@ -11,14 +19,59 @@ import tv.mapper.embellishcraft.util.WoodType;
 
 public class CustomBedBlock extends BedBlock
 {
-    private final WoodType wood;
-    private final DyeColor bedColor;
+    protected final WoodType wood;
+    protected final DyeColor bedColor;
+
+    protected static final VoxelShape head_w = Block.makeCuboidShape(0.0D, 9.0D, 0.0D, 2.0D, 16.0D, 16.0D);
+    protected static final VoxelShape head_e = Block.makeCuboidShape(14.0D, 9.0D, 0.0D, 16.0D, 16.0D, 16.0D);
+    protected static final VoxelShape head_s = Block.makeCuboidShape(0.0D, 9.0D, 14.0D, 16.0D, 16.0D, 16.0D);
+    protected static final VoxelShape head_n = Block.makeCuboidShape(0.0D, 9.0D, 0.0D, 16.0D, 16.0D, 2.0D);
+    protected static final VoxelShape foot_w = Block.makeCuboidShape(0.0D, 9.0D, 0.0D, 2.0D, 13.0D, 16.0D);
+    protected static final VoxelShape foot_e = Block.makeCuboidShape(14.0D, 9.0D, 0.0D, 16.0D, 13.0D, 16.0D);
+    protected static final VoxelShape foot_s = Block.makeCuboidShape(0.0D, 9.0D, 14.0D, 16.0D, 13.0D, 16.0D);
+    protected static final VoxelShape foot_n = Block.makeCuboidShape(0.0D, 9.0D, 0.0D, 16.0D, 13.0D, 2.0D);
 
     public CustomBedBlock(DyeColor colorIn, WoodType wood, Properties properties)
     {
         super(colorIn, properties);
         this.bedColor = colorIn;
         this.wood = wood;
+    }
+
+    @Override
+    public VoxelShape getShape(BlockState state, IBlockReader worldIn, BlockPos pos, ISelectionContext context)
+    {
+        Direction direction = state.get(HORIZONTAL_FACING);
+        Direction direction1 = state.get(PART) == BedPart.HEAD ? direction : direction.getOpposite();
+
+        if(state.get(PART) == BedPart.HEAD)
+        {
+            switch(direction1)
+            {
+                case NORTH:
+                    return VoxelShapes.or(head_n, field_220181_h);
+                case SOUTH:
+                    return VoxelShapes.or(head_s, field_220182_i);
+                case WEST:
+                    return VoxelShapes.or(head_w, field_220183_j);
+                default:
+                    return VoxelShapes.or(head_e, field_220184_k);
+            }
+        }
+        else
+        {
+            switch(direction1)
+            {
+                case NORTH:
+                    return VoxelShapes.or(foot_n, field_220181_h);
+                case SOUTH:
+                    return VoxelShapes.or(foot_s, field_220182_i);
+                case WEST:
+                    return VoxelShapes.or(foot_w, field_220183_j);
+                default:
+                    return VoxelShapes.or(foot_e, field_220184_k);
+            }
+        }
     }
 
     @Override
