1e45c11a90700308003edf239f89b735d55722d7
Mapper <killermapper@outlook.com>
2019-12-24 17:06:14 +0100

    Added crate screen.

--- src/main/java/tv/mapper/embellishcraft/client/gui/screen/inventory/CrateScreen.java
+++ src/main/java/tv/mapper/embellishcraft/client/gui/screen/inventory/CrateScreen.java
@@ -7,12 +7,16 @@ import net.minecraft.client.gui.screen.inventory.ContainerScreen;
 import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.text.ITextComponent;
+import tv.mapper.embellishcraft.Constants;
 import tv.mapper.embellishcraft.inventory.container.CrateContainer;
 
 public class CrateScreen extends ContainerScreen<CrateContainer> implements IHasContainer<CrateContainer>
 {
-    private static final ResourceLocation CRATE_SCREEN = new ResourceLocation("textures/gui/container/generic_54.png");
+    private static final ResourceLocation CRATE_SCREEN = new ResourceLocation(Constants.MODID, "textures/gui/crate.png");
     private final int inventoryRows;
+    
+    private static final int WIDTH = 176;
+    private static final int HEIGHT = 185;
 
     public CrateScreen(CrateContainer p_i51095_1_, PlayerInventory p_i51095_2_, ITextComponent p_i51095_3_)
     {
@@ -20,6 +24,14 @@ public class CrateScreen extends ContainerScreen<CrateContainer> implements IHas
         this.passEvents = false;
         this.inventoryRows = p_i51095_1_.getNumRows();
         this.ySize = 114 + this.inventoryRows * 18;
+    }
+    
+    protected void init()
+    {
+        super.init();
+
+        guiLeft = this.width / 2 - WIDTH / 2;
+        guiTop = this.height / 2 - HEIGHT / 2;
     }
 
     public void render(int p_render_1_, int p_render_2_, float p_render_3_)
@@ -45,9 +57,12 @@ public class CrateScreen extends ContainerScreen<CrateContainer> implements IHas
     {
         GlStateManager.color4f(1.0F, 1.0F, 1.0F, 1.0F);
         this.minecraft.getTextureManager().bindTexture(CRATE_SCREEN);
-        int i = (this.width - this.xSize) / 2;
-        int j = (this.height - this.ySize) / 2;
-        this.blit(i, j, 0, 0, this.xSize, this.inventoryRows * 18 + 17);
-        this.blit(i, j + this.inventoryRows * 18 + 17, 0, 126, this.xSize, 96);
+//        int i = (this.width - this.xSize) / 2;
+//        int j = (this.height - this.ySize) / 2;
+//        this.blit(i, j, 0, 0, this.xSize, this.inventoryRows * 18 + 17);
+//        this.blit(i, j + this.inventoryRows * 18 + 17, 0, 126, this.xSize, 96);
+        
+        this.blit(guiLeft, guiTop, 0, 0, WIDTH, HEIGHT);
+
     }
 }
