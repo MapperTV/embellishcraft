eff9807aa41f3c97f40acc1e88657e35e04d5e29
Mapper <killermapper@outlook.com>
2020-02-01 11:10:09 +0100

    Update Forge and mappings.

--- src/main/java/tv/mapper/embellishcraft/world/OreGenerator.java
+++ src/main/java/tv/mapper/embellishcraft/world/OreGenerator.java
@@ -17,39 +17,39 @@ public class OreGenerator
         for(Biome biome : ForgeRegistries.BIOMES)
         {
             if(CommonConfig.BASALT_GENERATION.get())
-                biome.addFeature(GenerationStage.Decoration.UNDERGROUND_ORES, Feature.ORE.func_225566_b_(
+                biome.addFeature(GenerationStage.Decoration.UNDERGROUND_ORES, Feature.ORE.withConfiguration(
                     new OreFeatureConfig(OreFeatureConfig.FillerBlockType.NATURAL_STONE, ModBlocks.BASALT.getDefaultState(), CommonConfig.BASALT_SIZE.get().intValue())).func_227228_a_(
                         Placement.COUNT_RANGE.func_227446_a_(
                             new CountRangeConfig(CommonConfig.BASALT_CHANCE.get(), CommonConfig.BASALT_MIN_HEIGHT.get(), CommonConfig.BASALT_MIN_HEIGHT.get(), CommonConfig.BASALT_MAX_HEIGHT.get()))));
 
             if(CommonConfig.SLATE_GENERATION.get())
                 biome.addFeature(GenerationStage.Decoration.UNDERGROUND_ORES,
-                    Feature.ORE.func_225566_b_(
+                    Feature.ORE.withConfiguration(
                         new OreFeatureConfig(OreFeatureConfig.FillerBlockType.NATURAL_STONE, ModBlocks.SLATE.getDefaultState(), CommonConfig.SLATE_SIZE.get().intValue())).func_227228_a_(
                             Placement.COUNT_RANGE.func_227446_a_(
                                 new CountRangeConfig(CommonConfig.SLATE_CHANCE.get(), CommonConfig.SLATE_MIN_HEIGHT.get(), CommonConfig.SLATE_MIN_HEIGHT.get(), CommonConfig.SLATE_MAX_HEIGHT.get()))));
 
             if(CommonConfig.MARBLE_GENERATION.get())
-                biome.addFeature(GenerationStage.Decoration.UNDERGROUND_ORES, Feature.ORE.func_225566_b_(
+                biome.addFeature(GenerationStage.Decoration.UNDERGROUND_ORES, Feature.ORE.withConfiguration(
                     new OreFeatureConfig(OreFeatureConfig.FillerBlockType.NATURAL_STONE, ModBlocks.MARBLE.getDefaultState(), CommonConfig.MARBLE_SIZE.get().intValue())).func_227228_a_(
                         Placement.COUNT_RANGE.func_227446_a_(
                             new CountRangeConfig(CommonConfig.MARBLE_CHANCE.get(), CommonConfig.MARBLE_MIN_HEIGHT.get(), CommonConfig.MARBLE_MIN_HEIGHT.get(), CommonConfig.MARBLE_MAX_HEIGHT.get()))));
 
             if(CommonConfig.GNEISS_GENERATION.get())
-                biome.addFeature(GenerationStage.Decoration.UNDERGROUND_ORES, Feature.ORE.func_225566_b_(
+                biome.addFeature(GenerationStage.Decoration.UNDERGROUND_ORES, Feature.ORE.withConfiguration(
                     new OreFeatureConfig(OreFeatureConfig.FillerBlockType.NATURAL_STONE, ModBlocks.GNEISS.getDefaultState(), CommonConfig.GNEISS_SIZE.get().intValue())).func_227228_a_(
                         Placement.COUNT_RANGE.func_227446_a_(
                             new CountRangeConfig(CommonConfig.GNEISS_CHANCE.get(), CommonConfig.GNEISS_MIN_HEIGHT.get(), CommonConfig.GNEISS_MIN_HEIGHT.get(), CommonConfig.GNEISS_MAX_HEIGHT.get()))));
 
             if(CommonConfig.JADE_GENERATION.get())
                 biome.addFeature(GenerationStage.Decoration.UNDERGROUND_ORES,
-                    Feature.ORE.func_225566_b_(
+                    Feature.ORE.withConfiguration(
                         new OreFeatureConfig(OreFeatureConfig.FillerBlockType.NATURAL_STONE, ModBlocks.JADE.getDefaultState(), CommonConfig.JADE_SIZE.get().intValue())).func_227228_a_(
                             Placement.COUNT_RANGE.func_227446_a_(
                                 new CountRangeConfig(CommonConfig.JADE_CHANCE.get(), CommonConfig.JADE_MIN_HEIGHT.get(), CommonConfig.JADE_MIN_HEIGHT.get(), CommonConfig.JADE_MAX_HEIGHT.get()))));
 
             if(CommonConfig.LARVIKITE_GENERATION.get())
-                biome.addFeature(GenerationStage.Decoration.UNDERGROUND_ORES, Feature.ORE.func_225566_b_(
+                biome.addFeature(GenerationStage.Decoration.UNDERGROUND_ORES, Feature.ORE.withConfiguration(
                     new OreFeatureConfig(OreFeatureConfig.FillerBlockType.NATURAL_STONE, ModBlocks.LARVIKITE.getDefaultState(), CommonConfig.LARVIKITE_SIZE.get().intValue())).func_227228_a_(
                         Placement.COUNT_RANGE.func_227446_a_(
                             new CountRangeConfig(CommonConfig.LARVIKITE_CHANCE.get(), CommonConfig.LARVIKITE_MIN_HEIGHT.get(), CommonConfig.LARVIKITE_MIN_HEIGHT.get(), CommonConfig.LARVIKITE_MAX_HEIGHT.get()))));
