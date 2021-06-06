45d127f3577d646a5ae6e92af084da581912b36a
Mapper <official.mapper@gmail.com>
2021-06-06 09:37:22 -0400

    Updating Forge and code.

--- src/main/java/tv/mapper/embellishcraft/data/gen/ECGenerators.java
+++ src/main/java/tv/mapper/embellishcraft/data/gen/ECGenerators.java
@@ -1,10 +1,6 @@
 package tv.mapper.embellishcraft.data.gen;
 
-import java.util.Collections;
-
 import net.minecraft.data.DataGenerator;
-import net.minecraft.resources.ResourcePackType;
-import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.common.data.ExistingFileHelper;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
@@ -20,33 +16,20 @@ public class ECGenerators
     public static void gatherData(GatherDataEvent event)
     {
         DataGenerator generator = event.getGenerator();
-        ExistingFileHelper delegate = event.getExistingFileHelper();
-
-        ExistingFileHelper helper = new ExistingFileHelper(Collections.emptyList(), true)
-        {
-            @Override
-            public boolean exists(ResourceLocation loc, ResourcePackType type, String pathSuffix, String pathPrefix)
-            {
-                if(loc.getNamespace() == "mapperbase")
-                {
-                    return true;
-                }
-                return delegate.exists(loc, type, pathSuffix, pathPrefix);
-            }
-        };
+        ExistingFileHelper existingFileHelper = event.getExistingFileHelper();
 
         generator.addProvider(new ECRecipes(generator));
         generator.addProvider(new ECStonecutterRecipes(generator));
 
         generator.addProvider(new ECLootTables(generator));
-        generator.addProvider(new ECBlockStates(generator, ECConstants.MODID, helper));
-        generator.addProvider(new ECBlockModels(generator, ECConstants.MODID, helper));
-        generator.addProvider(new ECItemModels(generator, ECConstants.MODID, helper));
+        generator.addProvider(new ECBlockStates(generator, ECConstants.MODID, existingFileHelper));
+        generator.addProvider(new ECBlockModels(generator, ECConstants.MODID, existingFileHelper));
+        generator.addProvider(new ECItemModels(generator, ECConstants.MODID, existingFileHelper));
 
-        ECBlockTags blockTags = new ECBlockTags(generator, helper);
+        ECBlockTags blockTags = new ECBlockTags(generator, existingFileHelper);
 
         generator.addProvider(blockTags);
-        generator.addProvider(new ECItemTags(generator, blockTags, helper));
+        generator.addProvider(new ECItemTags(generator, blockTags, existingFileHelper));
 
         generator.addProvider(new ECLang(generator, ECConstants.MODID, "en_us"));
         generator.addProvider(new ECLang(generator, ECConstants.MODID, "fr_fr"));
--- src/main/java/tv/mapper/embellishcraft/util/ModSoundType.java
+++ src/main/java/tv/mapper/embellishcraft/util/ModSoundType.java
@@ -1,13 +1,15 @@
 package tv.mapper.embellishcraft.util;
 
-import net.minecraft.block.SoundType;
-import net.minecraft.util.SoundEvent;
+import java.util.function.Supplier;
 
-public class ModSoundType extends SoundType
+import net.minecraft.util.SoundEvent;
+import net.minecraftforge.common.util.ForgeSoundType;
+
+public class ModSoundType extends ForgeSoundType
 {
-    public ModSoundType(float volumeIn, float pitchIn, SoundEvent breakSoundIn, SoundEvent stepSoundIn, SoundEvent placeSoundIn, SoundEvent hitSoundIn, SoundEvent fallSoundIn)
+
+    public ModSoundType(float volumeIn, float pitchIn, Supplier<SoundEvent> breakSoundIn, Supplier<SoundEvent> stepSoundIn, Supplier<SoundEvent> placeSoundIn, Supplier<SoundEvent> hitSoundIn, Supplier<SoundEvent> fallSoundIn)
     {
         super(volumeIn, pitchIn, breakSoundIn, stepSoundIn, placeSoundIn, hitSoundIn, fallSoundIn);
     }
-
 }
